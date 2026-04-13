db0299c89a5720131991adba5b09b9f3918e1583
muhammadfajrulsutagalang-dotcom <muhammadfajrulsutagalang@smkitihsanulfikri.sch.id>
2026-04-13 08:20:57 +0700

    comit 1 sistem bot suta

---
+++ src/user_handlers.py
@@ -0,0 +1,15 @@
+from telegram import Update
+from telegram.ext import ContextTypes, ConversationHandler
+from database import simpan_user, simpan_laporan, get_produk
+from utils.keyboards import main_menu, lapor_keyboard
+from config import ADMIN_ID
+
+LAPOR = 1
+
+JENIS_SAMPAH = """JENIS SAMPAH YANG DITERIMA:\n- Plastik (bersih & kering)\n- Botol\n- Sachet\n- Kertas"""
+
+HARGA_SAMPAH = """HARGA SAAT INI:\n• Plastik : Rp 2.000 / kg\n• Botol   : Rp 3.000 / kg"""
+
+JADWAL = "Pengambilan: Senin, Rabu, Jumat"
+JAM = "Jam operasional: 08.00 - 16.00 WIB"
+EDUKASI = """Pisahkan sampah organik dan non-organik."""
