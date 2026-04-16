b2a21f51a6ed5c0651ffc74d5035f108386d873b
Hanif Faiz Al Farisy <elffwhite@gmail.com>
2026-04-16 14:39:28 +0700

    Replace token and admin ID with placeholders
    Updated token and admin ID placeholders in config.

--- Src/config.py
+++ Src/config.py
@@ -1,6 +1,6 @@
 # config.py
-TOKEN = "8401179937:AAERrnXua9u16rzGEONmFCnmDahnugCQgkA"
-ADMIN_ID = 8694601310
+TOKEN = "masukkan token"
+ADMIN_ID = id
 
 BOT_NAME = "Sehati Waste Bot"
 VERSION = "1.0.0"
